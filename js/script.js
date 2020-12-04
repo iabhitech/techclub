9352e2465ae8f5363bfd84b2d61c24a00a2ca4fb
Abhineet Verma <198.abhineet@gmail.com>
2020-12-04 21:48:18 +0530

    Fixed Some Issues and Features

--- js/script.js
+++ js/script.js
@@ -7,15 +7,46 @@ $(document).ready(function(){
 });
 
 // Animations init
-new WOW().init();
+// new WOW().init();
 
 
   $(document).ready(() => {
     // SideNav Button Initialization
-    $(".button-collapse").sideNav();
-      // SideNav Scrollbar Initialization
-      var sideNavScrollbar = document.querySelector('.custom-scrollbar');
-      var ps = new PerfectScrollbar(sideNavScrollbar);
+    // $(".button-collapse").sideNav();
+    //   // SideNav Scrollbar Initialization
+    //   var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+    //   var ps = new PerfectScrollbar(sideNavScrollbar);
+
+    if(window.width < 1440){
+      $('body').append('<div class="drag-sidenav" style="left: 0px; width: 10px;"></div>');
+      $('.drag-sidenav').on('click',function(){
+
+      });
+      console.log('sidenav')
+      
+
+    }
 
 // new WOW().init();
 });
+
+
+
+
+
+
+
+
+
+$('#openSideNav').on('click', function(){
+    $('#slide-out-nav').css('transform','translate(0)');
+});
+
+$('.collapsible-header').on('click',function(){
+  $(this).hasClass('active')?($('.active').removeClass('active'), $('.collapsible-body').slideUp('fast')):($('.active').removeClass('active'),$(this.parentNode).toggleClass('active'),
+  $(this).toggleClass('active'), $('.collapsible-body').slideUp('fast'), $($(this.parentNode).find('.collapsible-body')).slideDown('fast'));
+  // $($(this.parentNode).find('.collapsible-body')).slideToggle("slow");
+
+  
+  
+})
