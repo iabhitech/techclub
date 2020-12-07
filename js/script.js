6737f137cb38eec8ffed9e268f4ff645d60d8442
Abhineet Verma <198.abhineet@gmail.com>
2020-12-08 00:17:01 +0530

    Removed Dummy text and fix css etc.

--- js/script.js
+++ js/script.js
@@ -10,43 +10,37 @@ $(document).ready(function(){
 // new WOW().init();
 
 
-  $(document).ready(() => {
-    // SideNav Button Initialization
-    // $(".button-collapse").sideNav();
-    //   // SideNav Scrollbar Initialization
-    //   var sideNavScrollbar = document.querySelector('.custom-scrollbar');
-    //   var ps = new PerfectScrollbar(sideNavScrollbar);
-
-    if(window.width < 1440){
-      $('body').append('<div class="drag-sidenav" style="left: 0px; width: 10px;"></div>');
+$(window).bind("load", function() { 
+  // insert your code here 
+  if(window.innerWidth < 1440){
+    $('main').append('<div class="drag-sidenav" style="position: fixed;left: 0;top:0; width:10px;height: 90vh;"></div>');
+    $('<div class="mask nav-mask"></div>').insertBefore('footer');
       $('.drag-sidenav').on('click',function(){
-
+        $('#openSideNav').trigger('click');
       });
-      console.log('sidenav')
-      
-
-    }
-
-// new WOW().init();
+      $('.nav-mask').on('click', function(){
+        $('#openSideNav').trigger('click');
+      });
+  }
 });
 
 
 
-
-
-
-
-
-
 $('#openSideNav').on('click', function(){
-    $('#slide-out-nav').css('transform','translate(0)');
+      $('#slide-out-nav').hasClass('nav-expanded')? 
+      ($('#slide-out-nav').css('transform', 'translateX(-241px)').toggleClass('nav-expanded'),$('.nav-mask').removeClass('nav-mask-active')):  
+      ($('#slide-out-nav').css('transform','translateX(0px)').toggleClass('nav-expanded'),$('.nav-mask').addClass('nav-mask-active'))
 });
 
 $('.collapsible-header').on('click',function(){
   $(this).hasClass('active')?($('.active').removeClass('active'), $('.collapsible-body').slideUp('fast')):($('.active').removeClass('active'),$(this.parentNode).toggleClass('active'),
   $(this).toggleClass('active'), $('.collapsible-body').slideUp('fast'), $($(this.parentNode).find('.collapsible-body')).slideDown('fast'));
   // $($(this.parentNode).find('.collapsible-body')).slideToggle("slow");
+});
 
-  
-  
-})
+function register(){
+  $('#myTab a[href="#register"]').trigger('click');
+}
+function login(){
+  $('#myTab a[href="#signIn"]').trigger('click');
+}
